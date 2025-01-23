55d78c315b8930728c83448ab3d9b978d3a9135b
Sanisorn Yoskorn <syoskorn@myseneca.ca>
2025-01-23 01:51:17 -0500

    Added lab2d

---
+++ test_if.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if True:
+    print('This print is a part of the if statement')
+
+if False:
+    print('This first print statement will never run')
+    print('This second print statement will also not run')
+print('This print statement will run')
