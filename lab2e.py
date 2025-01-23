de924c3f784ab669637d4b4f4a73448fe7da3581
Sanisorn Yoskorn <syoskorn@myseneca.ca>
2025-01-23 02:04:47 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Your Full Name
+# Author ID: your_seneca_id
+# Date Created: yyyy/mm/dd
+
+timer = 10  # Initialize the countdown timer
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1
+print('blast off!')
